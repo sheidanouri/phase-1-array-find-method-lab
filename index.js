8de2ef0d12bd0664c22f89e8e15aee4b1f3f4c5c
sheida noori <sheida.nouri@gmail.com>
2023-03-24 23:04:38 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+
+function superbowlWin( record ){
+    for (const user of record) {
+        if (user.result === "W"){
+            return user.year
+        }
+       
+        
+    }
+    return undefined
+
+}
+
